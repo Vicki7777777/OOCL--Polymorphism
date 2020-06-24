9c01af143e1b1cd6168618d4900f2aa01d05276d
Vicki7777777 <43415417+Vicki7777777@users.noreply.github.com>
2020-06-24 15:53:47 +0800

    OOCL--Polymorphism
    OOCL--Polymorphism

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Printer;
+
+public class Main {
+    public static void main(String[] args) {
+    	Printer printer1 = new Printer();
+    	printer1.Function("printer1");
+    	Printer printer2 = new Color();
+    	printer2.Function("printer2");
+    	Printer printer3 = new BlackWhite();
+    	printer3.Function("printer3");
+    	
+    	
+    }
+}
